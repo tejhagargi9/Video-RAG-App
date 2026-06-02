c762fb859334a709ffe50e4408231d816ee0bbef
tejhagargi9 <tejhagargigit@gmail.com>
2026-06-02 11:29:53 +0530

    added frontend host url to backend cors

--- Backend/main.py
+++ Backend/main.py
@@ -31,7 +31,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://video-rag-app-dgow.vercel.app", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
