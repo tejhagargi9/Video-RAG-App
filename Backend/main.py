6cb2915a06f9a4092aa34d4a9b37c71354d047fb
tejhagargi9 <tejhagargigit@gmail.com>
2026-06-02 10:49:02 +0530

    added all other  dependencies to requirements

--- Backend/main.py
+++ Backend/main.py
@@ -31,7 +31,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", "http://127.0.0.1:5174"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
